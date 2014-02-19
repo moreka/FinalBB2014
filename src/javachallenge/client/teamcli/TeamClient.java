5b33b81c834ed984736c93c3afc46be53d93f5a1
merhdad <mehrdad29>
2014-02-19 22:38:28 +0330

    findPath!

--- src/javachallenge/client/teamcli/TeamClient.java
+++ src/javachallenge/client/teamcli/TeamClient.java
@@ -1,14 +1,39 @@
 package javachallenge.client.teamcli;
 
 import javachallenge.client.Client;
+import javachallenge.exceptions.CellIsNullException;
 import javachallenge.util.Cell;
 import javachallenge.util.Direction;
-
-import java.util.ArrayList;
+import javachallenge.util.Edge;
+import javachallenge.util.EdgeType;
 
 public class TeamClient extends Client {
 
-    public ArrayList<Direction> findPath(Cell source, Cell destinartion) {
+    private boolean isDFSArrive = false;
+    boolean[][] flags = new boolean[map.getSizeX()][map.getSizeY()];
+
+    private Direction findPath(Cell source, Cell destination) {
+        Direction[] dir = Direction.values();
+        flags[source.getX()][source.getY()] = true;
+        outer: for (int i = 0; i < 6; i++) {
+            try {
+                Cell neighborCell = map.getNeighborCell(source, dir[i]);
+                Edge neighborEdge = source.getEdge(dir[i]);
+                if (neighborCell != null && flags[neighborCell.getX()][neighborCell.getY()] == false &&
+                        neighborEdge.getType() == EdgeType.OPEN && neighborCell.isGround()) {
+                    if (neighborCell.equals(destination)) {
+                        isDFSArrive = true;
+                        return dir[i];
+                    }
+                    else
+                        findPath(neighborCell, destination);
+                    if (isDFSArrive)
+                        return dir[i];
+                }
+                } catch (CellIsNullException e) {
+                    e.printStackTrace();
+                }
+            }
         return null;
     }
 
